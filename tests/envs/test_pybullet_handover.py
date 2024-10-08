5f48fc4e16e3ae2fcd1c9144a27254f802858bb2
Tom Silver <tomssilver@gmail.com>
2024-10-08 14:42:33 -0400

    wip

--- tests/envs/test_pybullet_handover.py
+++ tests/envs/test_pybullet_handover.py
@@ -12,9 +12,8 @@ def test_pybullet_handover():
     """Tests for pybullet_handover.py."""
     intake_horizon = 5
     task = PyBulletHandoverTask(
-        "task0",
-        intake_horizon,
-        _use_gui=True,
+        intake_horizon=intake_horizon,
+        use_gui=True,
     )
     mdp = task.mdp
     rng = np.random.default_rng(123)
