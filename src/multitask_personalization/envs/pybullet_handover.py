f65a0298cdc2feceeb593d439f5fd0c8af606d22
Tom Silver <tomssilver@gmail.com>
2024-10-08 12:42:43 -0400

    starting handover task

---
+++ src/multitask_personalization/envs/pybullet_handover.py
@@ -0,0 +1,137 @@
+"""A handover environment implemented in PyBullet."""
+
+from dataclasses import dataclass
+from typing import Any, TypeAlias
+from pybullet_helpers.joint import JointPositions
+from pybullet_helpers.geometry import Pose, Pose3D
+
+import numpy as np
+from numpy.typing import NDArray
+
+from multitask_personalization.envs.intake_process import IntakeProcess
+from multitask_personalization.envs.mdp import MDP
+from multitask_personalization.envs.task import Task
+from multitask_personalization.structs import (
+    CategoricalDistribution,
+    Image,
+)
+
+
+@dataclass(frozen=True)
+class _HandoverState:
+    """A state in the handover environment."""
+
+    robot_base: Pose
+    robot_joints: JointPositions
+    human_base: Pose
+    human_joints: JointPositions
+
+
+_HandoverAction: TypeAlias = JointPositions | None  # None = ready for handover
+
+
+class PyBulletHandoverMDP(MDP[_HandoverState, _HandoverAction]):
+    """A handover environment implemented in PyBullet."""
+
+    def __init__(self) -> None:
+        # TODO load environment.
+        import ipdb; ipdb.set_trace()
+
+    @property
+    def state_space(self) -> set[_HandoverState]:
+        raise NotImplementedError("State space too large to enumerate")
+
+    @property
+    def action_space(self) -> set[_HandoverAction]:
+        raise NotImplementedError("Action space too large to enumerate")
+
+    def state_is_terminal(self, state: _HandoverState) -> bool:
+        # TODO terminate if human reaches object.
+        import ipdb; ipdb.set_trace()
+
+    def get_reward(
+        self, state: _HandoverState, action: _HandoverAction, next_state: _HandoverState
+    ) -> float:
+        if self.state_is_terminal(next_state):
+            return 1.0
+        return 0.0
+
+    def get_initial_state_distribution(
+        self,
+    ) -> CategoricalDistribution[_HandoverState]:
+        raise NotImplementedError("Initial state distribution too large")
+    
+    def sample_initial_state(self, rng: np.random.Generator) -> _HandoverState:
+        # TODO randomize robot and human initial positions
+        import ipdb; ipdb.set_trace()
+
+    def get_transition_distribution(
+        self, state: _HandoverState, action: _HandoverAction
+    ) -> CategoricalDistribution[_HandoverState]:
+        # TODO implement deterministic transition distribution
+        import ipdb; ipdb.set_trace()
+
+    def render_state(self, state: _HandoverState) -> Image:
+        # TODO reset state and take image
+        import ipdb; ipdb.set_trace()
+
+
+@dataclass(frozen=True)
+class _ROMReachableQuestion:
+    """Ask the person to try to reach a certain position."""
+
+    position: Pose3D  # in absolute coordinates
+
+    def __lt__(self, other: Any) -> bool:
+        return str(self) < str(other)
+
+_HandoverIntakeObs: TypeAlias = bool  # whether or not reaching is successful
+_HandoverIntakeAction: TypeAlias = _ROMReachableQuestion
+
+
+class PyBulletHandoverIntakeProcess(IntakeProcess[_HandoverIntakeObs, _HandoverIntakeAction]):
+    """Intake process for the pybullet handover environment."""
+
+    def __init__(self, horizon: int) -> None:
+        self._horizon = horizon
+
+        # TODO load environment.
+        import ipdb; ipdb.set_trace()
+
+    @property
+    def observation_space(self) -> set[_HandoverIntakeObs]:
+        return {True, False}
+
+    @property
+    def action_space(self) -> set[_HandoverIntakeAction]:
+        raise NotImplementedError("Action space too large to enumerate")
+
+    @property
+    def horizon(self) -> int:
+        return self._horizon
+
+    def get_observation_distribution(
+        self,
+        action: _HandoverIntakeAction,
+    ) -> CategoricalDistribution[_HandoverIntakeObs]:
+        # TODO use ground truth ROM model to check
+        import ipdb; ipdb.set_trace()
+
+
+@dataclass
+class PyBulletHandoverTask(Task):
+    """The full handover task."""
+
+    _id: str
+
+    @property
+    def id(self) -> str:
+        return self._id
+
+    @property
+    def mdp(self) -> PyBulletHandoverMDP:
+        return PyBulletHandoverMDP()
+
+    @property
+    def intake_process(self) -> PyBulletHandoverIntakeProcess:
+        return PyBulletHandoverIntakeProcess()
