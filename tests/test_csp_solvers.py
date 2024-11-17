842be62652b7596ab57e69e52361b492f58db6de
Tom Silver <tomssilver@gmail.com>
2024-11-17 10:02:43 -0500

    wip

--- tests/test_csp_solvers.py
+++ tests/test_csp_solvers.py
@@ -2,19 +2,25 @@
 
 import gymnasium as gym
 import numpy as np
+from tomsutils.spaces import EnumSpace
 
-from multitask_personalization.csp_solvers import RandomWalkCSPSolver
+from multitask_personalization.csp_solvers import (
+    ExhaustiveDiscreteCSPSolver,
+    RandomWalkCSPSolver,
+)
 from multitask_personalization.structs import (
     CSP,
+    CSPCost,
     CSPVariable,
+    DiscreteCSP,
     FunctionalCSPConstraint,
     FunctionalCSPSampler,
     LogProbCSPConstraint,
 )
 
 
-def test_solve_csp():
-    """Tests for csp_solvers.py."""
+def test_hybrid_csp_solvers():
+    """Tests for discrete-continuous CSP solvers."""
     x = CSPVariable("x", gym.spaces.Box(0, 1, dtype=np.float_))
     y = CSPVariable("y", gym.spaces.Box(0, 1, dtype=np.float_))
     z = CSPVariable("z", gym.spaces.Discrete(5))
@@ -40,3 +46,25 @@ def test_solve_csp():
     assert sol is not None
     assert sol[x] < sol[y]
     assert sol[y] < sol[z] / 5
+
+
+def test_discrete_csp_solvers():
+    """Tests for discrete CSP solvers."""
+    a = CSPVariable("a", EnumSpace(list(range(8))))
+    b = CSPVariable("b", EnumSpace(list(range(8))))
+    c = CSPVariable("c", EnumSpace(list(range(8))))
+
+    # Optimal: a = 2, b = 3, c = 0.
+    c1 = FunctionalCSPConstraint("c1", [a, b], lambda a, b: a + b <= 5)
+    c2 = FunctionalCSPConstraint("c2", [a, b], lambda a, b: a < b)
+    c3 = FunctionalCSPConstraint("c3", [c], lambda c: c <= 5)
+    cost = CSPCost("cost", [a, b, c], lambda a, b, c: -(a * b * (10 - c)))
+
+    csp = DiscreteCSP([a, b, c], [c1, c2, c3], cost)
+
+    solver = ExhaustiveDiscreteCSPSolver(show_progress_bar=False)
+    sol = solver.solve(csp)
+    assert sol is not None
+    assert sol[a] == 2
+    assert sol[b] == 3
+    assert sol[c] == 0
