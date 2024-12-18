f04864e57d0a297f18120db4b8b5e38c6a205952
Tom Silver <tomssilver@gmail.com>
2024-12-18 14:03:16 -0500

    wip

--- src/multitask_personalization/envs/cooking/cooking_csp.py
+++ src/multitask_personalization/envs/cooking/cooking_csp.py
@@ -45,31 +45,91 @@ class CookingCSPGenerator(CSPGenerator[CookingState, CookingAction]):
         self,
         obs: CookingState,
     ) -> tuple[list[CSPVariable], dict[CSPVariable, Any]]:
+        
         # One per ingredient with: is_used, quantity, pot_id, start_time.
         ingredients = sorted(obs.ingredients)
         variables = [
             CSPVariable(ingredient, Box(0, np.inf, (4,), dtype=np.float_))
             for ingredient in ingredients
         ]
-        return variables
+
+        # Initialization.
+        initialization = {
+            v: np.zeros(4, dtype=np.float_)
+            for v in variables
+        }
+
+        return variables, initialization
 
     def _generate_personal_constraints(
         self,
         obs: CookingState,
         variables: list[CSPVariable],
     ) -> list[CSPConstraint]:
-        import ipdb
+        
+        # Final ingredients must comprise some happy meal.
+        def _is_happy_meal(
+            *ingredients: NDArray,
+        ) -> bool:
+            import ipdb; ipdb.set_trace()
 
-        ipdb.set_trace()
+        happy_meal_constraint = FunctionalCSPConstraint(
+            "happy_meal_constraint",
+            variables,
+            _is_happy_meal,
+        )
+
+        return [happy_meal_constraint]
+
 
     def _generate_nonpersonal_constraints(
         self,
         obs: CookingState,
         variables: list[CSPVariable],
     ) -> list[CSPConstraint]:
-        import ipdb
+        
+        constraints: list[CSPConstraint] = []
+        
+        # Pots cannot overlap on the stove.
+        def _pots_nonoverlapping(ingredient1: NDArray, ingredient2: NDArray) -> bool:
+            import ipdb; ipdb.set_trace()
 
-        ipdb.set_trace()
+        for i, ingredient1 in enumerate(variables[:-1]):
+            for ingredient2 in variables[i+1:]:
+                constraint = FunctionalCSPConstraint(
+                    f"{ingredient1.name}-{ingredient2.name}-nonoverlapping",
+                    [ingredient1, ingredient2],
+                    _pots_nonoverlapping
+                )
+                constraints.append(constraint)
+
+        # Ingredients cannot be in the same pot.
+        def _ingredients_different_pots(ingredient1: NDArray, ingredient2: NDArray) -> bool:
+            import ipdb; ipdb.set_trace()
+
+        for i, ingredient1 in enumerate(variables[:-1]):
+            for ingredient2 in variables[i+1:]:
+                constraint = FunctionalCSPConstraint(
+                    f"{ingredient1.name}-{ingredient2.name}-different-pots",
+                    [ingredient1, ingredient2],
+                    _ingredients_different_pots
+                )
+                constraints.append(constraint)
+
+        # Ingredient quantity used must be not more than total available.
+        def _ingredient_quantity_exists(ingredient: NDArray) -> bool:
+            import ipdb; ipdb.set_trace()
+
+        for ingredient in variables:
+            constraint = FunctionalCSPConstraint(
+                f"{ingredient.name}-quantity-exists",
+                [ingredient],
+                _ingredient_quantity_exists
+            )
+            constraints.append(constraint)
+
+        return constraints
+        
 
     def _generate_exploit_cost(
         self,
