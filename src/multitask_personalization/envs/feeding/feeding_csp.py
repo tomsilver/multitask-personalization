479eae36fff3349d7a92ba4692628f424ede1221
Tom Silver <tomssilver@gmail.com>
2025-04-25 09:39:08 -0400

    add warning to catch occlusion issue

--- src/multitask_personalization/envs/feeding/feeding_csp.py
+++ src/multitask_personalization/envs/feeding/feeding_csp.py
@@ -626,6 +626,9 @@ class FeedingCSPGenerator(CSPGenerator[FeedingObservation, FeedingAction]):
                     plate_score = self._get_plate_occlusion_score(plate_pose.position[:2], point_of_interest)
                     assert plate_score is not None, "Shouldn't be possible if IK is checked during constraint solving..."
                     plate_label = feedback["plate_occlusion"]
+                    if plate_label and np.isclose(plate_score, 0.0):
+                        print("OH NO!!!! We are screwed. User said there was occlusion when our model thinks none is possible.")
+                        import ipdb; ipdb.set_trace()
                     X.append(plate_score)
                     Y.append(plate_label)
 
@@ -633,6 +636,9 @@ class FeedingCSPGenerator(CSPGenerator[FeedingObservation, FeedingAction]):
                     drink_score = self._get_drink_occlusion_score(drink_pose.position[:2], point_of_interest)
                     assert drink_score is not None, "Shouldn't be possible if IK is checked during constraint solving..."
                     drink_label = feedback["drink_occlusion"]
+                    if drink_label and np.isclose(drink_score, 0.0):
+                        print("OH NO!!!! We are screwed. User said there was occlusion when our model thinks none is possible.")
+                        import ipdb; ipdb.set_trace()
                     X.append(drink_score)
                     Y.append(drink_label)
 
