466f960d7de4f934f37eaa9456a5e8a6fd62eb2f
RKJenamani <rajatkj11@gmail.com>
2025-04-26 14:10:27 -0400

    changes to pregen

--- src/multitask_personalization/feast_dummy.py
+++ src/multitask_personalization/feast_dummy.py
@@ -27,10 +27,10 @@ class Meal:
 
 MEALS = [
     Meal(1, "personal", "rectangular table", ["french fries"], ["ketchup", "ranch dressing"]),
-    Meal(2, "social with friend on left", "circular table", ["celery", "apple slices"], ["ranch dressing"]),
-    Meal(3, "watching TV in front", "circular table", ["steak", "potatoes"], []),
-    Meal(4, "personal", "circular table", ["pear slices"], ["ranch dressing"]),
-    Meal(5, "social TV-watching (with TV in front) and with friend on left side", "rectangular table", ["chicken nuggets"], ["ketchup", "ranch dressing"]),
+    Meal(2, "social with friend on left", "circular table", ["carrot sticks"], ["ranch dressing", "hummus"]),
+    Meal(3, "watching TV in front", "circular table", ["potato wedges"], ["ketchup", "ranch dressing"]),
+    Meal(4, "personal", "circular table", ["celery sticks"], ["ranch dressing", "hummus"]),
+    Meal(5, "social TV-watching (with TV in front) and with friend on left side", "rectangular table", ["tater tots"], ["ketchup", "ranch dressing"]),
 ]
 
 # Helper function to generate all possible bite orderings.
